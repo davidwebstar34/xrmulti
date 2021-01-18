c90a104aef9d8d549b79ed65a4c81b7ad85b2b8c
David <dwebster182@gmail.com>
2021-01-18 19:55:04 +0100

    reactjs amplify project integrated with sumerian

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+ import './App.css';
+ import { withAuthenticator, SumerianScene  } from 'aws-amplify-react';
+ import Amplify from 'aws-amplify';
+ import Aws_exports from './aws-exports';
+ import '@aws-amplify/ui/dist/style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ Amplify.configure(Aws_exports);
 
-export default App;
+ class App extends Component {
+   render() {
+     return (
+       <div style={ { height: '100vh' } }>
+         <SumerianScene sceneName='xrmulti'/>
+       </div>
+     );
+   }
+ };
+
+ export default withAuthenticator(App, { includeGreetings: true });
